06e8f2c0ef16fab07efe9c748945da2a8dccbd36
nctisz <m.angelodev@gmail.com>
2022-07-20 18:39:31 -0300

    Finaliza exercicio de Váriaveis

--- fundamentos/bloco-04-intro-ao-javascript/dia-01-primeiros-passos-javascript/Variaveis/Variaveis.js
+++ fundamentos/bloco-04-intro-ao-javascript/dia-01-primeiros-passos-javascript/Variaveis/Variaveis.js
@@ -1,5 +1,15 @@
-let myname = "Miguel";
+const myname = "Miguel";
 const birthCity = "Brasilia";
-const birthYear = 2001;
+let birthYear = 2001;
 
 console.log (myname, birthCity, birthYear);
+
+birthYear = 2030;
+
+console.log (birthYear);
+
+// A parte abaixo é onde pedem pra testar o motivo do erro, o que é claro já que birthCity é uma constante...
+
+// birthCity = "Amazonas"
+
+// console.log (birthCity)
