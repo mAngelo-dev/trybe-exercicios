feb72f80693bc1df71fb880c9e2c6383e7f23ddb
nctisz <m.angelodev@gmail.com>
2022-07-20 20:37:39 -0300

    if else exercicio

---
+++ fundamentos/bloco-04-intro-ao-javascript/dia-01-primeiros-passos-javascript/If&Else/If-e-else.js
@@ -0,0 +1,20 @@
+// ARROW FUNCTION E FOR EACH - O FOR EACH É UM ARRAY FUNCTION, NOTA É A ATRIBUIÇÃO DESSES 3 VALORES DENTRO DO ARRAY QUE RODAM DENTRO DE UMA CONDIÇÃO IF/ELSE
+[63, 20, 83].forEach(function (nota) {
+        if (nota >= 80) {
+            console.log("Parabéns que carninha gostosa!");
+        }
+        else if (nota > 60) {
+            console.log("Essa carne está faltando tempero");
+        }
+        else {
+            console.log("Tá ruim!");
+        }
+    })
+// for (i; i < notas.length; i++) {
+//     console.log(notas[i])   
+//     nota = notas[i]
+
+
+
+
+
