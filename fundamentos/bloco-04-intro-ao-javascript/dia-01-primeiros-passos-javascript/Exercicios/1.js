667c16e0728db5792b8085671455c06d3f17649c
nctisz <m.angelodev@gmail.com>
2022-07-21 14:10:31 -0300

    Adciona Ex.1

---
+++ fundamentos/bloco-04-intro-ao-javascript/dia-01-primeiros-passos-javascript/Exercicios/1.js
@@ -0,0 +1,8 @@
+const x = 22;
+const y = 10;
+
+console.log('Adição: ' + (x + y));
+console.log('Subtração: ' + (x - y));
+console.log('Multiplicação: ' + (x * y));
+console.log('Divisão: ' + (x / y));
+console.log('Resto(?): ' + (x % y));
