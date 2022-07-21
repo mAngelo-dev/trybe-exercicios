9f28fe853f263143235eb57c99fa34561af774ed
nctisz <m.angelodev@gmail.com>
2022-07-21 15:04:46 -0300

    Adiciona exercicio 3

--- fundamentos/bloco-04-intro-ao-javascript/dia-01-primeiros-passos-javascript/Exercicios/2.js
+++ fundamentos/bloco-04-intro-ao-javascript/dia-01-primeiros-passos-javascript/Exercicios/2.js
@@ -5,5 +5,5 @@ if (a > b ){
     console.log("'a' é maior que 'b'");
 }
 else {
-    console.log("'b' é maior que 'a'")
-}
+    console.log("'b' é maior que 'a'");
+};
---
+++ fundamentos/bloco-04-intro-ao-javascript/dia-01-primeiros-passos-javascript/Exercicios/3.js
@@ -0,0 +1,13 @@
+const a = 10;
+const b = 2;
+const c = 11;
+
+if (a > b && a > c) {
+    console.log("O maior numero é o: " + a + ' (a)');
+}
+else if (b > a && b > c) {
+    console.log("O maior numero é o: " + b + ' (b)');
+}
+else {
+    console.log("O maior numero é o: " + c + ' (c)');
+}
