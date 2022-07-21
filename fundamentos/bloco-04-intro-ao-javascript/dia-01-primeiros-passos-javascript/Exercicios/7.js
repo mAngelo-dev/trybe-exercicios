f75c963baacd5466e428ed92fb97893f6afe6501
nctisz <m.angelodev@gmail.com>
2022-07-21 19:32:42 -0300

    Exercicio 7 completo

---
+++ fundamentos/bloco-04-intro-ao-javascript/dia-01-primeiros-passos-javascript/Exercicios/7.js
@@ -0,0 +1,24 @@
+const percentage = 100;
+
+if (percentage > 100 || percentage < 0) {
+    console.log("Error")
+}
+else if (percentage >= 90) {
+    console.log("A");
+}
+else if (percentage >= 80) {
+    console.log("B");
+}
+else if (percentage >= 70) {
+    console.log("C");
+}
+else if (percentage >= 60) {
+    console.log("D");
+}
+else if (percentage >= 50) {
+    console.log("E");
+}
+else {
+    console.log("F")
+}
+
