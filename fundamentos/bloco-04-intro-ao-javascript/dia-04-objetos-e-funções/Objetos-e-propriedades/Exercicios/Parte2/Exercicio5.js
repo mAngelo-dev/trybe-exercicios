47ef3b225c4926458c3a3af80ca1e2e9b7cb4fd5
nctisz <m.angelodev@gmail.com>
2022-07-25 23:54:36 -0300

    Conclui exercicio 6 e adiciona a base do 5

---
+++ fundamentos/bloco-04-intro-ao-javascript/dia-04-objetos-e-funções/Objetos-e-propriedades/Exercicios/Parte2/Exercicio5.js
@@ -0,0 +1,4 @@
+//Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+let arrayDeInteiros = [2, 3, 2, 5, 8, 2, 3];
+
+//Criar condição caso o numero investigado seja igual a ele mesmo, acrescente um contador que irá retornar o maior dos resultados após verificar os numeros.
