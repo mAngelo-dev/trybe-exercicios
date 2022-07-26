c251a75f7f236113a81b1fd502765e14db214a95
nctisz <m.angelodev@gmail.com>
2022-07-25 23:09:49 -0300

    Adiciona exercicio e corrige erro no Pratice5

---
+++ fundamentos/bloco-04-intro-ao-javascript/dia-04-objetos-e-funções/Objetos-e-propriedades/Exercicios/Parte2/Exercicio2.js
@@ -0,0 +1,13 @@
+let numbers = [2, 3, 6, 7, 10, 1];
+let maiorNumero = 0;
+
+function verifyBiggestNumber(n) {
+    for (let index = 0; index < numbers.length; index++) {
+        if (numbers[index] > maiorNumero) {
+            maiorNumero = numbers[index]
+        }
+    }
+    return maiorNumero   
+}
+
+console.log(verifyBiggestNumber(numbers))
