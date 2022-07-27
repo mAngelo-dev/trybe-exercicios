b4b1f751d6ce01926d9e8838da7a0fa27cd332f1
nctisz <m.angelodev@gmail.com>
2022-07-27 18:32:40 -0300

    Adiciona até exercicio de Seletores

---
+++ fundamentos/bloco-05-DOM-Eventos-WebStorage/dia-01-DOM-e-Seletores/Exercicios/script.js
@@ -0,0 +1,5 @@
+let container = document.querySelector('#container');
+container.style.backgroundColor = 'green';
+let cabecalhos = document.querySelectorAll(".emergency-tasks");
+cabecalhos.style.backgroundColor = 'black';
+cabecalhos.style.borderRadius = '20%';
---
+++ fundamentos/bloco-05-DOM-Eventos-WebStorage/dia-01-DOM-e-Seletores/Seletores-de-objetos/scriptPratice2.js
@@ -0,0 +1,13 @@
+let allParagrafos = document.getElementsByClassName("paragrafos");
+
+// for (let index = 0; index < allParagrafos.length; index++) {
+//   console.log(allParagrafos[index]);
+//   allParagrafos[index].innerText = 'Mandioca'
+// }
+
+allParagrafos[0].innerText = 'Hello, world!'
+allParagrafos[0].style.color = '#711ea8'
+
+let subtitulo = document.getElementsByTagName('h4')
+subtitulo[0].innerText = 'Very nice!'
+subtitulo[0].style.backgroundColor = '#711ea8'
