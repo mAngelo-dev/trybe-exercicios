8fa67047db113fd327e4cfee59c91e05bf4e7cf4
nctisz <m.angelodev@gmail.com>
2022-07-27 15:43:38 -0300

    Começa o dia 1 do módulo 5

---
+++ fundamentos/bloco-05-DOM-Eventos-WebStorage/dia-01-DOM-e-Seletores/Seletores-de-objetos/script.js
@@ -0,0 +1,13 @@
+const titulo = document.getElementById('page-title');
+titulo.innerText = 'Esse titulo mostra que eu vinculei certo'
+
+const paragrafo = document.getElementById('paragraph');
+paragrafo.style.color = '#24949c';
+paragrafo.innerText = 'Essa cor foi personalizada por Miguel!'
+
+const subtitulo = document.getElementById('subtitle');
+subtitulo.innerText = 'Olha o avanço na programação vindo aí!'
+
+const segundoParagrafo = document.getElementById('second-paragraph');
+segundoParagrafo.innerText = 'Meu deus, meu senhor'
+segundoParagrafo.style.color = '#24949c'
